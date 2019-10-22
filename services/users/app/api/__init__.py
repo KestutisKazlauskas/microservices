0b1c90ca78daf8d59fc78ad4ae9102c91bb61b38
KestutisKazlauskas <kstutisk4@gmail.com>
2019-10-22 23:55:39 +0300

    User creation(not finished).

--- services/users/app/api/__init__.py
+++ services/users/app/api/__init__.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
+
+from app import db
+from app.api.models import User
 
 user_blueprint = Blueprint('users', __name__)
 api = Api(user_blueprint)
@@ -14,6 +17,19 @@ class UserPingView(Resource):
             'message': 'pong!'
         }
 
+class UserList(Resource):
 
+    @staticmethod
+    def post():
+        # TODO make validation and serialization
+        data = request.get_json()
+        user = User(email=data.get('email'), username=data.get('username'))
+        db.session.add(user)
+        db.session.commit()
+
+        return {'username': user.username, 'email': user.email}, 201
+
+
+api.add_resource(UserList, '/users/')
 api.add_resource(UserPingView, '/users/ping/')
 
--- services/users/app/tests/test_users.py
+++ services/users/app/tests/test_users.py
@@ -14,5 +14,52 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_create_user(self):
+        """Test user creation to the database"""
+        request_data = {
+            'username': 'kestutis',
+            'email': 'kestutis@test.com'
+        }
+        with self.client:
+            response = self.client.post(
+                '/users/',
+                data=json.dumps(request_data),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            # check if response is created
+            self.assertEqual(response.status_code, 201)
+            # check all data values
+            for key in request_data:
+                self.assertIn(data[key], request_data[key])
+
+    def test_required_data_send(self):
+        """Test validation without a required data"""
+        required_data = {
+            'username': 'kestutis',
+            'email': 'kestutis@test.com'
+        }
+        with self.client:
+            for key in required_data:
+
+                data = required_data.copy()
+                del data[key]
+
+                response = self.client.post(
+                    '/users/',
+                    data=json.dumps(data),
+                    content_type='application/json'
+                )
+                data = json.loads(response.data.decode())
+                
+                self.assertEqual(response.status_code, 400)
+                self.assertEqual(data['message'], f'{key} is required!')
+
+    def test_incorrect_email(self):
+        pass
+
+    def test_duplicated_user_by_email(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
